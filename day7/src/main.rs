df47bec5070ad9127f6686c110dd3b14086edb49
proMario <mario.lucas@prosense.co.za>
2018-12-07 18:08:34 +0200

    more changes going home

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -19,11 +19,11 @@ Step F must be finished before step E can begin.";
     assert_eq!(part1(_test_input), "CABDFE");
     println!("Part1 {:?}", part1(input));
 
-    assert_eq!(part2(_test_input, 64, 1), 15);
-    println!("Part2 {:?}", part2(input, 4, 4));
+    assert_eq!(part2(_test_input, 64, 2), 15);
+    println!("Part2 {:?}", part2(input, 4, 5));
 }
 
-fn part2(input: &str, delay: u8, helpers: u8) -> usize {
+fn part2(input: &str, delay: u8, helpers: usize) -> usize {
     let mut nodes: HashMap<char, (Vec<char>, u8)> = HashMap::new();
     input.lines().for_each(|x| {
         let new_char = x.chars().nth(5).unwrap();
@@ -32,10 +32,11 @@ fn part2(input: &str, delay: u8, helpers: u8) -> usize {
             .or_insert((vec![], new_char as u8 - delay));
         entry.0.push(x.chars().nth(36).unwrap());
     });
-    println!("{:?}", nodes);
-    // let prosp_nodes = find_first_node(&nodes);
-    // traverse(nodes, prosp_nodes);
-    unimplemented!();
+
+    let base_nodes = find_first_node(&nodes);
+    let mut working_nodes = base_nodes.into_iter().take(helpers).collect();
+    let mut prosp_nodes = base_nodes.into_iter().skip(helpers).collect();
+    work(nodes, working_nodes, prosp_nodes, helpers)
 }
 
 fn part1(input: &str) -> String {
@@ -50,7 +51,7 @@ fn part1(input: &str) -> String {
     traverse(nodes, prosp_nodes)
 }
 
-fn find_first_node(nodes: &HashMap<char, (Vec<char>, bool)>) -> Vec<char> {
+fn find_first_node<T>(nodes: &HashMap<char, (Vec<char>, T)>) -> Vec<char> {
     let mut first_nodes = nodes.keys().fold(vec![], |mut acc, x| {
         if nodes.values().map(|x| &x.0).flatten().all(|y| y != x) {
             acc.push(*x)
@@ -59,6 +60,58 @@ fn find_first_node(nodes: &HashMap<char, (Vec<char>, bool)>) -> Vec<char> {
     });
     first_nodes.sort_unstable();
     first_nodes
+}
+
+fn work(
+    mut nodes: HashMap<char, (Vec<char>, u8)>,
+    mut working_nodes: Vec<char>,
+    mut prosp_nodes: Vec<char>,
+    helpers: usize,
+) -> usize {
+    let mut time = 0;
+    loop {
+        for working_node in working_nodes {
+            nodes.entry(working_node).and_modify(|c| c.1 -= 1);
+        }
+        if let Some(new_workers) =
+            find_next_workers(&mut nodes, &mut working_nodes, &mut prosp_nodes)
+        {
+            for new_worker in new_workers {
+                if working_nodes.len() < helpers {
+                    working_nodes.push(new_worker);
+                } else {
+                    prosp_nodes.push(new_worker);
+                }
+            }
+        } else {
+            break;
+        }
+        time += 1
+    }
+    time
+}
+
+fn find_next_workers(
+    nodes: &mut HashMap<char, (Vec<char>, u8)>,
+    working_nodes: &mut Vec<char>,
+    prosp_nodes: &mut Vec<char>,
+) -> Option<Vec<char>> {
+    prosp_nodes.append(&mut nodes.get(&curr_char).unwrap_or(&(vec![], 0)).0.clone());
+    prosp_nodes.sort_unstable();
+    prosp_nodes.dedup();
+    let mut results = None;
+    for next_alpha in working_nodes {
+        if let Some(new_worker) = nodes
+            .values()
+            .find(|aa| aa.0.contains(&next_alpha) || aa.1 == 0)
+        {
+            if results.is_none() {
+                results = Some(vec![]);
+            }
+            results.unwrap().push(next_alpha.clone())
+        }
+    }
+    results
 }
 
 fn traverse(mut nodes: HashMap<char, (Vec<char>, bool)>, mut prosp_nodes: Vec<char>) -> String {
