c6c5710a940553d1e5b9f3c364a8a9a443bc0321
mZbZ <lucas.mario@gmail.com>
2018-12-12 13:37:04 +0200

    day8 & day 11 done
    Rebased authors

---
+++ day11/src/main.rs
@@ -0,0 +1,73 @@
+#![feature(nll)]
+
+extern crate elapsed;
+extern crate rayon;
+
+use elapsed::measure_time;
+use rayon::prelude::*;
+
+fn main() {
+    let input = 9306;
+
+    assert_eq!(calc_power_level(3, 5, 8), 4);
+
+    assert_eq!(part1(42), ((21, 61), 3, 30));
+    assert_eq!(part1(18), ((33, 45), 3, 29));
+    println!("Part1 {:?}", part1(input));
+
+    assert_eq!(part2(18), ((90, 269), 16, 113));
+    assert_eq!(part2(42), ((232, 251), 12, 119));
+
+    println!("Part2 {:?}", part2(input));
+}
+
+fn part2(input: usize) -> ((usize, usize), usize, isize) {
+    let (elapsed, output) = measure_time(|| {
+        (1..300usize)
+            .into_par_iter()
+            .map(|ss| shared(input, ss))
+            .max_by_key(|&x| x.2)
+            .unwrap()
+    });
+    println!("Part2 Time Elapsed = {}", elapsed);
+    output
+}
+
+fn part1(input: usize) -> ((usize, usize), usize, isize) {
+    let (elapsed, output) = measure_time(|| shared(input, 3));
+    println!("Part1 Time Elapsed = {}", elapsed);
+    output
+}
+
+fn shared(input: usize, sample_size: usize) -> ((usize, usize), usize, isize) {
+    let grid = (1..=300)
+        .map(|x| {
+            (1..=300)
+                .map(|y| calc_power_level(x, y, input))
+                .collect::<Vec<isize>>()
+        })
+        .collect::<Vec<Vec<isize>>>();
+
+    let mut max = ((0, 0), sample_size, 0);
+    for x in 0..(grid.len() - sample_size) {
+        for y in 0..(grid.len() - sample_size) {
+            let total: isize = grid
+                .iter()
+                .skip(x)
+                .take(sample_size)
+                .fold(0isize, |sum, xx| {
+                    sum + xx.iter().skip(y).take(sample_size).sum::<isize>()
+                });
+            if total > max.2 {
+                max = ((x + 1, y + 1), sample_size, total)
+            }
+        }
+    }
+    max
+}
+
+fn calc_power_level(x: usize, y: usize, serial: usize) -> isize {
+    let mut result = (((x + 10) * y) + serial) * (x + 10);
+    result -= result % 100;
+    ((result / 100) % 10) as isize - 5
+}
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,102 @@
+#![feature(nll)]
+
+extern crate elapsed;
+extern crate regex;
+
+use elapsed::measure_time;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+
+    assert_eq!(part1("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"), 138);
+    println!("Part1 {:?}", part1(input));
+
+    assert_eq!(part2("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"), 66);
+
+    println!("Part2 {:?}", part2(input));
+}
+
+fn part2(input: &str) -> usize {
+    let (elapsed, output) = measure_time(|| {
+        let input_v: Vec<usize> = input
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+
+        get_node_part2(&input_v, &mut 0, &mut None)
+    });
+    println!("Part2 Time Elapsed = {}", elapsed);
+    output
+}
+
+fn part1(input: &str) -> usize {
+    let (elapsed, output) = measure_time(|| {
+        let input_v: Vec<usize> = input
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+
+        get_node(&input_v, &mut 0, &mut 0)
+    });
+    println!("Part1 Time Elapsed = {}", elapsed);
+    output
+}
+
+fn get_node(input: &[usize], idx: &mut usize, metadata: &mut usize) -> usize {
+    // println!("Found group at {}", *idx);
+    let mut c_nodes = input[*idx];
+    *idx += 1;
+    let mut m_nodes = input[*idx];
+    *idx += 1;
+    while c_nodes > 0 {
+        get_node(input, idx, metadata);
+        c_nodes -= 1;
+    }
+    while m_nodes > 0 {
+        *metadata += input[*idx];
+        *idx += 1;
+        m_nodes -= 1;
+    }
+    *metadata
+}
+
+fn get_node_part2(input: &[usize], idx: &mut usize, child_nodes: &mut Option<Vec<usize>>) -> usize {
+    let mut c_nodes = input[*idx];
+    *idx += 1;
+    let mut m_nodes = input[*idx];
+    *idx += 1;
+    let mut metadata = 0;
+    if c_nodes == 0 {
+        while m_nodes > 0 {
+            metadata += input[*idx];
+            *idx += 1;
+            m_nodes -= 1;
+        }
+        match child_nodes {
+            Some(child_nodes_vec) => child_nodes_vec.push(metadata),
+            None => panic!("Should never happen"),
+        }
+    } else {
+        let mut new_child_nodes = Some(vec![]);
+
+        while c_nodes > 0 {
+            get_node_part2(input, idx, &mut new_child_nodes);
+            c_nodes -= 1;
+        }
+
+        let mut child_idx = vec![];
+        while m_nodes > 0 {
+            child_idx.push(input[*idx] - 1);
+            *idx += 1;
+            m_nodes -= 1;
+        }
+
+        for idx in child_idx {
+            metadata += new_child_nodes.as_ref().unwrap().get(idx).unwrap_or(&0);
+        }
+
+        child_nodes.as_mut().unwrap_or(&mut vec![]).push(metadata);
+    }
+
+    metadata
 }
